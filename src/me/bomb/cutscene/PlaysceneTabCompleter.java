cce7f304df849cfd287bcd8243d35cc660fe4851
FatSaw <eklesosek@gmail.com>
2022-04-20 02:21:27 +0300

    Own class for playscene tabcomplete

---
+++ src/me/bomb/cutscene/PlaysceneTabCompleter.java
@@ -0,0 +1,60 @@
+package me.bomb.cutscene;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+public class PlaysceneTabCompleter implements TabCompleter {
+	
+	@Override
+	public ArrayList<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		ArrayList<String> tabcomplete = new ArrayList<String>();
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (player.hasPermission("cutscene.playscene") || player.hasPermission("cutscene.playscene.other")) {
+				if (args.length == 1) {
+					for (String routename : Cutscene.routedata.getKeys(false)) {
+						if (routename.startsWith(args[0]) && ((Cutscene.routedata.contains(routename + ".world") && Cutscene.routedata.isString(routename + ".world") && player.getWorld().getName().equals(Cutscene.routedata.getString(routename + ".world"))) || player.hasPermission("cutscene.playscene.other"))) {
+							tabcomplete.add(routename);
+						}
+					}
+				} else if (args.length == 2) {
+					if ("normal".startsWith(args[1].toLowerCase())) tabcomplete.add("normal");
+					if ("green".startsWith(args[1].toLowerCase())) tabcomplete.add("green");
+					if ("negative".startsWith(args[1].toLowerCase())) tabcomplete.add("negative");
+					if ("split".startsWith(args[1].toLowerCase())) tabcomplete.add("split");
+				} else if (args.length == 3) {
+					if (player.hasPermission("cutscene.playscene.other")) {
+						for (Player tcplayer : Bukkit.getOnlinePlayers()) {
+							if (!player.getUniqueId().equals(tcplayer.getUniqueId()) && tcplayer.getName().toLowerCase().startsWith(args[2].toLowerCase())) {
+								tabcomplete.add(tcplayer.getName());
+							}	
+						}
+					}
+				}
+			}
+		} else {
+			if (args.length == 1) {
+				for (String routename : Cutscene.routedata.getKeys(false)) {
+					if (routename.startsWith(args[0]))
+						tabcomplete.add(routename);
+				}
+			} else if (args.length == 2) {
+				if ("normal".startsWith(args[1].toLowerCase())) tabcomplete.add("normal");
+				if ("green".startsWith(args[1].toLowerCase())) tabcomplete.add("green");
+				if ("negative".startsWith(args[1].toLowerCase())) tabcomplete.add("negative");
+				if ("split".startsWith(args[1].toLowerCase())) tabcomplete.add("split");
+			} else if (args.length == 3) {
+				for (Player tcplayer : Bukkit.getOnlinePlayers()) {
+					if (tcplayer.getName().toLowerCase().startsWith(args[2].toLowerCase())) tabcomplete.add(tcplayer.getName());
+				}
+			}
+		}
+		return tabcomplete;
+	};
+	
+}
